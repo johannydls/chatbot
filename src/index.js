d8ed9bc29955f36883d44358e3715fefb59b3657
Johanny LS <johanny.dls@gmail.com>
2019-06-19 00:51:21 -0300

    Add message webhook

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const verifyWebhook = require('./verify-webhook');
+const messageWebhook = require('./message-webhook');
 
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     res.send('Server on');
 });
 
 app.get('/webhook', verifyWebhook);
+app.post('/webhook', messageWebhook);
+
+
 
 app.listen(PORT, () => console.log(`Express server is listening on port ${PORT}`));
